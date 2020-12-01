8c2d7999d634ec2154c34dcd67b28a9e74b39cbd
EgorKononov <egor.academ@gmail.com>
2020-12-01 13:39:28 +0700

    Исправлена задача Shapes

--- Shapes/src/ru/academits/java/kononov/shapes/Triangle.java
+++ Shapes/src/ru/academits/java/kononov/shapes/Triangle.java
@@ -76,7 +76,7 @@ public class Triangle implements Shape {
         return Math.max(y1, Math.max(y2, y3)) - Math.min(y1, Math.min(y2, y3));
     }
 
-    private double getSideLength(double x1, double y1, double x2, double y2) {
+    private static double getSideLength(double x1, double y1, double x2, double y2) {
         return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
     }
 
@@ -102,7 +102,7 @@ public class Triangle implements Shape {
 
     @Override
     public String toString() {
-        return "Triangle (x1 = " + x1 + ", y1 = " + y1 + ", x2 = " + x2 + ", y2 = " + y2 + ", x3 = " + x3 + ", y3 = " + y3 + ", Area = "
+        return "Triangle ((" + x1 + "; " + y1 + "), (" + x2 + "; " + y2 + "), (" + x3 + "; " + y3 + "), Area = "
                 + getArea() + ", Perimeter = " + getPerimeter() + ")";
     }
 
