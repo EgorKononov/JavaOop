ad81c32782011f52af2f0f794d4a75bf0cf188bb
EgorKononov <egor.academ@gmail.com>
2021-01-16 23:45:46 +0700

    Исправлена задача ArrayListHome

--- ArrayListHome/src/ru/academits/java/kononov/array_list_home/Main.java
+++ ArrayListHome/src/ru/academits/java/kononov/array_list_home/Main.java
@@ -21,36 +21,26 @@ public class Main {
         return stringsFromFile;
     }
 
-    public static void removeEvenNumbersFromArrayList(ArrayList<Integer> arrayList) {
-        for (int i = 0; i < arrayList.size(); i++) {
-            if (arrayList.get(i) % 2 == 0) {
-                arrayList.remove(i);
+    public static void removeEvenNumbersFromAList(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) % 2 == 0) {
+                list.remove(i);
                 --i;
             }
         }
     }
 
-    public static ArrayList<Integer> getArrayListWithoutRepeatedNumbers(ArrayList<Integer> arrayList) {
-        ArrayList<Integer> arrayListWithoutRepeatedNumbers = new ArrayList<>();
+    public static ArrayList<Integer> getListWithoutRepeatedNumbers(ArrayList<Integer> list) {
+        ArrayList<Integer> listWithoutRepeatedNumbers = new ArrayList<>(list.size());
 
-        for (Integer e1 : arrayList) {
-            boolean isNumberRepeat = false;
-
-            for (Integer e2 : arrayListWithoutRepeatedNumbers) {
-                if (e1.equals(e2)) {
-                    isNumberRepeat = true;
-                    break;
-                }
-            }
-
-            if(!isNumberRepeat){
-                arrayListWithoutRepeatedNumbers.add(e1);
+        for (Integer e : list) {
+            if (!listWithoutRepeatedNumbers.contains(e)) {
+                listWithoutRepeatedNumbers.add(e);
             }
         }
 
-        return arrayListWithoutRepeatedNumbers;
+        return listWithoutRepeatedNumbers;
     }
-
 
     public static void main(String[] args) {
         ArrayList<String> stringsFromFile = getStringsFromFile("ArrayListHome\\Input.txt");
@@ -61,16 +51,16 @@ public class Main {
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
         System.out.println("Исходный список на массиве:");
         System.out.println(numbers);
-        removeEvenNumbersFromArrayList(numbers);
+        removeEvenNumbersFromAList(numbers);
         System.out.println("Список на массиве без четных чисел:");
         System.out.println(numbers);
         System.out.println();
 
-        ArrayList<Integer> arrayListWithRepeatedNumbers = new ArrayList<>(Arrays.asList(1, 2, 2, 3, 4, 5, 2, 6, 7, 1, 8, 9, 5, 10));
+        ArrayList<Integer> listWithRepeatedNumbers = new ArrayList<>(Arrays.asList(1, 2, 2, 3, 4, 5, 2, 6, 7, 1, 8, 9, 5, 10));
         System.out.println("Исходный список на массиве:");
-        System.out.println(arrayListWithRepeatedNumbers);
-        ArrayList<Integer> arrayListWithoutRepeatedNumbers = getArrayListWithoutRepeatedNumbers(arrayListWithRepeatedNumbers);
+        System.out.println(listWithRepeatedNumbers);
+        ArrayList<Integer> listWithoutRepeatedNumbers = getListWithoutRepeatedNumbers(listWithRepeatedNumbers);
         System.out.println("Список на массиве без повторяющихся чисел:");
-        System.out.println(arrayListWithoutRepeatedNumbers);
+        System.out.println(listWithoutRepeatedNumbers);
     }
 }
